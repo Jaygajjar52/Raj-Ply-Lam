8698c96521e7ccdda6adab9a980c4a34364106ae
Jay <jereces2404@gmail.com>
2026-08-23 14:16:47 +0530

    Fix brand image imports

--- client/src/Brands/index.ts
+++ client/src/Brands/index.ts
@@ -1,10 +1,10 @@
 import Century from "./Century Laminates.png";
-import Virgo from "./Virgo Laminates Logo.png"
+import Virgo from "./Virgo Laminates Logo.png";
 import Greenpanel from "./Greenpanel.png";
 import RoyalTouche from "./Royal Touche Logo.png";
 import Timex from "./Timex MICA LOGO.png";
 import Vita from "./Vita Hardware.png";
-import ActionTESA from "./Action TESA Logo.png";
+import ActionTESA from "./Action Tesa Logo.png";
 import CrossBond from "./Crossbond.png";
 
 export {
