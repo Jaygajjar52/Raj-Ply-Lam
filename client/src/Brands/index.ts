f3ef6c15763f45ed24fadb1f608900b61ec63d11
Jay <jereces2404@gmail.com>
2026-08-23 14:13:50 +0530

    Fix Timex logo import

--- client/src/Brands/index.ts
+++ client/src/Brands/index.ts
@@ -2,7 +2,7 @@ import Century from "./Century Laminates.png";
 import Virgo from "./Virgo Laminates Logo.png"
 import Greenpanel from "./Greenpanel.png";
 import RoyalTouche from "./Royal Touche Logo.png";
-import Timex from "./Timex MICA Logo.png";
+import Timex from "./Timex MICA LOGO.png";
 import Vita from "./Vita Hardware.png";
 import ActionTESA from "./Action TESA Logo.png";
 import CrossBond from "./Crossbond.png";
